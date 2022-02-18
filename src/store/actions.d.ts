4094bdcbf83222e3451d72086865aaa627a083ea
Jordanoo <jordanhernandez86@gmail.com>
2022-02-18 14:04:41 +0100

    add user type

---
+++ src/store/actions.d.ts
@@ -0,0 +1,25 @@
+export enum UserActionType {
+  LOGIN = "LOGIN",
+  LOGOUT = "LOGOUT",
+  ERROR = "ERROR",
+  SUCCESS = "SUCCESS",
+  FIELD = "FIELD",
+}
+
+export type UserAction =
+  | {
+      type:
+        | UserActionType.LOGIN
+        | UserActionType.LOGOUT
+        | UserActionType.ERROR
+        | UserActionType.SUCCESS;
+    }
+  | {
+      type: UserActionType.FIELD;
+      field: string;
+      value: string;
+    };
+
+export type UserProviderProps = {
+  children?: React.ReactNode;
+};
